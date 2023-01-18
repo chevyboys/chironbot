2eee192e3a5058a1307cd01a5b52a399f8d0c7a4
chevyboys <volcanicchevy@gmail.com>
2023-01-17 18:32:30 -0700

    fix double registering of message component handle

--- src/Classes/ModuleManager.ts
+++ src/Classes/ModuleManager.ts
@@ -116,6 +116,8 @@ export class ModuleManager extends Array<IChironModule> implements IModuleManage
                             this.client.on(Events.MessageCreate, (input) => { component.exec(input) })
                             this.client.on(Events.MessageUpdate, (input) => { component.exec(input) })
                             messageCommands.push(component);
+                        } else if (component instanceof MessageComponentInteractionComponent) {
+                            //let the interaction handler register these, don't do anything
                         }
                         else {
                             this.client.on(component.trigger, (input) => { component.exec(input) })
