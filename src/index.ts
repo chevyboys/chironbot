ae9ba9e26bdfef02b88f78499f52f4659935f2f2
chevyboys <volcanicchevy@gmail.com>
2023-05-26 21:06:21 -0600

    add graceful shutdown for scheduler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export * from "./Classes/ChironClient"
 export * from "./Classes/ChironConfig"
 export * from "./Classes/Module"
+
+import * as schedule from "node-schedule"
+process.on('SIGINT', function () {
+    schedule.gracefulShutdown()
+        .then(() => process.exit(0))
+})
