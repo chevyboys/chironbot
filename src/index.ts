c0258d3f9901416997161d8a8e731257ada0fc8d
chevyboys <volcanicchevy@gmail.com>
2023-05-26 21:14:02 -0600

    Formatting update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./Classes/ChironConfig"
 export * from "./Classes/Module"
 
 import * as schedule from "node-schedule"
+
 process.on('SIGINT', function () {
     schedule.gracefulShutdown()
         .then(() => process.exit(0))
